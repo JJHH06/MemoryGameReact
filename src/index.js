be7b8cdebaa80226d81e48a0d4f4d4de5f485501
Jose Hurtarte <JJHH121106@gmail.com>
2021-03-28 16:18:11 -0600

    first containers configuration

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import "./style.scss"
 import "bootstrap/dist/js/bootstrap"
 import React from 'react';
 import ReactDom from 'react-dom';
+import Game from './Game.js';
 
+ReactDom.render(
+    <React.StrictMode>
+        <Game/>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
