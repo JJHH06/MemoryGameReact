475925b1cc0fb2586e8eaab3aa7f84785c1b7e3f
Jose Hurtarte <JJHH121106@gmail.com>
2021-03-29 02:40:57 -0600

    game logic complete
    Only card logic is left

--- src/Game.js
+++ src/Game.js
@@ -12,6 +12,7 @@ import img7 from './assets/meme7.jpg'
 import img8 from './assets/meme8.png'
 
 //creacion de objetos de memoria
+//no es necesaria una key, ya que siempre se le hara
 const cartas = [
     {
         src: img1,
@@ -79,20 +80,94 @@ const cartas = [
     },
 ]
 
-function Game(){
-    const [cards, setCards]  = useState([]);
+//funcion de desordenamiento
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
     
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+//desordena las cartas
+shuffle(cartas);
+
+
+function Game(){
+    //el mapa de las cartas
+    const [cards, setCards]  = useState([]);
+
+    //la primera y la segunda carta seleccionadas
+    const [carta1, setCarta1] = useState({});
+    const [carta2, setCarta2] = useState({});
+    const [puntaje, setPuntaje] = useState(0);
+    const [winnedCards, setWinnedCards] = useState([]);
+
+    const cardIsFlippable = (id, casilla) =>{
+        //primero se ve si la primera y si esta existe
+        if(!(Object.keys(carta1).length === 0 && carta1.constructor === Object)&&((casilla === carta1.casilla && id === carta1.id))){
+            
+            return false;
+        }
+        else if(Object.keys(carta1).length === 0 && carta1.constructor === Object){
+            setCarta1({id,casilla});
+        }
+        else if(Object.keys(carta2).length === 0 && carta2.constructor === Object){
+            setCarta2({id,casilla});
+        }
+        return true;
+    }
+
+    //va a colocar las cartas recien ganadas
+    const addWinnedCards= () =>{
+        setWinnedCards([carta1.casilla, carta2.casilla]);
+    }
+
+    const addNoWinnerCard = () =>{
+        setWinnedCards([]);
+    }
+
+    const checkMatch = () =>{
+        if(!(Object.keys(carta2).length === 0 && carta2.constructor === Object)){//si el cambio no fue porque se seteo a vacio
+            if(carta2.id ===carta1.id){//aqui significa un nuevo punto
+                setPuntaje(puntaje+1);//se agrega al puntaje
+                addWinnedCards();
+            }
+            else{//aqui significa que no adivino
+                addNoWinnerCard();
+            }
+        }
+        
+    }
+
+    
+
+    //esto es para que se haga una sola vez
     useEffect(() => {
         setCards(cartas);
     },[])
+    
 
+    //cuando haya una segunda carta seleccionada
+    useEffect(()=>{
+        checkMatch();
+    },[carta2])
+
+    //alert('El pepe')
     return (
         <div className='game'>
             {/* <h1 className = 'title'>hola xd</h1> */}
             <div className='game-container'>
                 {
-                    cards.map((card) =>(
-                        <Card />
+                    cards.map((card,index) =>(
+                        <Card id={card.id} casilla={index} imagen = {card.src} cardIsFlippable = {cardIsFlippable} winnedCards={winnedCards}/>
                     ))
                 }
             </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,22 @@ import ReactCardFlip from 'react-card-flip';
 import fondo from '../assets/background.png';
 
 
-const Card = ({imagen, casilla, id}) =>{
+const Card = ({imagen, casilla, id, cardIsFlippable}) =>{
     const [isRevealed, setRevealed] = useState(false);
+
+    const clickListener = e =>{
+        //esto va a hacer que se revierta siempre el boolean de estado
+        if(cardIsFlippable(id, casilla)){
+            setRevealed(!isRevealed)
+        }
+    }
+
     return (
         <div className = 'card'>
-            <h2>Carta</h2>
+            <ReactCardFlip isFlipped = {isRevealed}>
+                <img className='memory-image' src = {fondo} alt = 'fondo' onClick ={clickListener}/>
+                <img className='memory-image' src = {imagen} alt = 'imagen'onClick ={clickListener}/>
+            </ReactCardFlip>
         </div>
     )
 }
