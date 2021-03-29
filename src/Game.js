43982e99f86baceb4392cfdc108752c509a74ed5
Jose Hurtarte <JJHH121106@gmail.com>
2021-03-28 18:43:04 -0600

    First card creation inside container

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.scss';
+import Card from './components/Card';
 
 
 function Game(){
-
+    const [cards, setCards]  = useState([1,2,3,4]);
     return (
         <div className='game'>
+            {/* <h1 className = 'title'>hola xd</h1> */}
             <div className='game-container'>
-                <h1>hola xd</h1>
+                {
+                    cards.map((card) =>(
+                        <Card />
+                    ))
+                }
             </div>
         </div>
     );
